5d65125e9c1de396648e25f07403ae75b99283bf
m-abrams <max.abrams@menloschool.org>
2024-11-18 09:38:33 -0800

    Finished the card class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
-
+// Card Class by Max Abrams
 public class Card {
+    // Declaring instance variables
+    private String rank;
+    private String suit;
+    private int value;
+
+    // Card Constructor
+    public Card(String theRank, String theSuit, int theValue) {
+        rank = theRank;
+        suit = theSuit;
+        value = theValue;
+    }
+
+    // Setter Methods
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    // Getter Methods
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    // ToString Method
+    public String toString() {
+        return this.rank + " of " + this.suit;
+    }
 }
